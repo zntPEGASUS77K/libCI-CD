66e3ae3f44f6ed2e260666adfa926f2d7278f496
michael <michael.ramanampamonjy@ethumada.com>
2025-07-11 22:57:45 +0300

    Enhance: First commit

---
+++ src/main/java/com/gitlabci/libci/LibCiApplication.java
@@ -0,0 +1,13 @@
+package com.gitlabci.libci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gitlabci/libci/LibCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlabci.libci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
